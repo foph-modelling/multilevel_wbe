c91667a5229664d19dec8fa2fe0b809b36234028
jriou <julien.riou@ispm.unibe.ch>
2023-05-31 18:16:10 +0200

    Run a3

--- main.R
+++ main.R
@@ -13,8 +13,8 @@ data_date = "2023-03-14" # name of the repertory with fixed data (up-to-date was
 data_path = "../../02_data/wastewater/"
 period_dates = c("2022-05-16","2022-09-05","2023-01-02") # set at the lowest points between waves, on Mondays so weeks are not cut
 controls = list(update_data=FALSE, # set to TRUE before sourcing to update the data
-                rerun_models=FALSE, # only applies to large models
-                compute_cv=TRUE, 
+                rerun_models=TRUE, # only applies to large models
+                compute_cv=FALSE, 
                 data_date=data_date,
                 analysis_date=analysis_date,
                 data_path=data_path,
