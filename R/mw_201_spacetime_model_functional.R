c291b5879966005e35edf6a6b2d5589c9ef8841d
jmunday <jamesdanielmunday@gmail.com>
2023-12-11 12:26:23 +0100

    reset to previous version of spacetime model debug - server version now in "_server"

--- R/mw_201_spacetime_model_functional.R
+++ R/mw_201_spacetime_model_functional.R
@@ -22,39 +22,9 @@
 # scp UBELIX:/storage/homefs/jr18s506/projects/multilevel_wbe/savepoints/savepoint_2023-05-15/ma5.3.3.rds savepoints/savepoint_2023-05-15/. 
 # scp UBELIX:/storage/homefs/jr18s506/projects/multilevel_wbe/savepoints/savepoint_2023-05-15/ma5.4.1.rds savepoints/savepoint_2023-05-15/. 
 
-#source("R/setup.R")
-
-library(data.table)
-
-library(dplyr     )   
-library(readr)
-library(forcats   )
-library(stringr )
-library(ggplot2   )
-library(tibble  )
-library(lubridate )  
-library(tidyr)
-library(purrr)
-
-library(lubridate)
-library(ISOweek)
-library(INLA)
-library(sf)
-library(splines)
-library(cowplot)
-library(spdep)
-library(jsonlite)
-library(scales,)
-library(units)
+source("R/setup.R")
 
 
-
-source('R/mw_009_spacetime_model_functions.R')
-source('R/mw_011_evaluation_functions.R')
-source('R/mw_008_load_pop_covars.R')
-
-
-message("sourced functions")
 
 if( !dir.exists('outputs')){
   dir.create('outputs')
@@ -65,9 +35,6 @@ save.point = paste0('outputs/last_run_', Sys.time())
 dir.create(save.point)
 ww1 = readRDS(fs::path(controls$savepoint,"ww1.rds"))
 shapes = readRDS(fs::path(controls$savepoint,"shapes.rds"))
-
-
-message("Loaded data")
 
 #' 
 #' We now model all ARAs together. 
@@ -94,25 +61,31 @@ ww_all = ww1 %>%
 saveRDS(ww_all,file=paste0(save.point,"/ww_all.rds"))
 
 
-ww_all = ww_all %>% filter(date > lubridate::ymd(20220210) & date < lubridate::ymd(20220330))
+ww_all = ww_all %>% filter(date > lubridate::ymd(20220210) & date < lubridate::ymd(20220331))
 #ww_all = ww_all %>% filter(day1<20)
 
 ww_all = ww_all %>% complete(ara_id, day)
 
 ww_all$day1 = ww_all$day
 
-message("Combined ww data and covariates")
+
 # correspondence table
-#corr_all = ww_all %>% 
-#  group_by(ara_n,ara_id,ara_name,kt,pop,lab,lab_n,lab2,lab_n2,lab_method,lab_method_n,ara1,ara2,NUTS2_name) %>% 
-#  count() %>% 
-#  ungroup() 
-#corr_all_ara = ww_all %>% 
-#  group_by(ara1,ara_name,ara_id,kt,NUTS2_name) %>% 
-#  count() %>% 
-#  ungroup() 
+corr_all = ww_all %>% 
+  group_by(ara_n,ara_id,ara_name,kt,pop,lab,lab_n,lab2,lab_n2,lab_method,lab_method_n,ara1,ara2,NUTS2_name) %>% 
+  count() %>% 
+  ungroup() 
+corr_all_ara = ww_all %>% 
+  group_by(ara1,ara_name,ara_id,kt,NUTS2_name) %>% 
+  count() %>% 
+  ungroup() 
 #saveRDS(corr_all_ara,file=paste0("../",controls$savepoint,"corr_all_ara.rds"))
 
+if(!controls$rerun_models) {
+  mw_100_desc_table(ww_all) %>%
+    dplyr::mutate(across(everything(),as.character)) %>%
+    tidyr::gather() %>%
+    flextable::flextable(cwidth=c(4,4))
+}
 
 # Construct spatial model inputs
 
@@ -120,9 +93,6 @@ message("Combined ww data and covariates")
 catchments = shapes$ara_shp
 catchments = st_transform(catchments, 25830)
 catchment_centroids = st_centroid(catchments)
-
-
-message("Loaded shape data")
 
 # merge ww data with centroids to ensure geometries map properly
 
@@ -139,11 +109,6 @@ catchment_centroids = merge(catchment_centroids, ww_all, by='ara_id', how='right
 centroid_coords = st_coordinates(catchment_centroids)
 colnames(centroid_coords) = c('X1', 'Y1')
 
-message("Prepared inputs")
-
-
-message("Running INLA model ... (expect a long pause)")
-
 # construct and run inla model in fit_inla_model() - in mw_009_spacetime_model_funtions.r
 inla_results = fit_inla_model(wwdata = ww_all, 
                               catchment_centroids = catchment_centroids, 
@@ -152,15 +117,13 @@ inla_results = fit_inla_model(wwdata = ww_all,
                               )
 
 
-message("Loading spatial data for projection")
-
 plz_pos = get_plz_centroids(crs_required = 25830)
 plz_coords = cbind(st_drop_geometry(plz_pos[,c('PLZ')]), st_coordinates(plz_pos) )
 
 plz_area = get_plz_areas(crs_required = 25830)
 
 plz_covariate_matrix = mw_008_load_pop_covars(scale = 'PLZ')
-message("Loaded spatial data for projection")
+
 
 time_steps = seq(1, length(unique(ww_all$day)))
 
@@ -171,14 +134,7 @@ for(time in time_steps){
   pcoords = rbind(pcoords, pcoords_date)
 }
 names(pcoords) <- c("PLZ", "x", "y", "time")
-
-message("Generated container for samples")
-message("Preparing projection covariates")
 pred_coords_covars = merge(unique(pcoords), unique(plz_covariate_matrix), by=c('PLZ'), how='left')
-message("Prepared projection covariates")
-
-
-message("Sampling the INLA model...")
 
 covariates = c('u20', 'o65', 'nec', 'pop_dens')
 get_samples_from_inla_model(inla_results = inla_results, 
@@ -187,10 +143,6 @@ get_samples_from_inla_model(inla_results = inla_results,
                             nsims = 500, 
                             model_dir = save.point)
 
-message(paste0("Sampling complete... outputs saved at ", save.point))
-
-message("Scoring samples...")
-
 scores = score_by_catch(nsims = 500,
                          savepath=save.point,
                          pred_coords_covars = pred_coords_covars, 
@@ -198,9 +150,7 @@ scores = score_by_catch(nsims = 500,
                          suffix='', 
                          log_vals=T, 
                          buffer=0)
-message("Scores generated")
 
-message("Plotting outputs and saving")
 
 scores$all_catch_res_long[, ':='(pred_mean=mean(prediction), upper=quantile(prediction, 0.95, na.rm=T), lower=quantile(prediction, 0.05, na.rm=T)), by=c('time', 'ara_id', 'model')]
 
@@ -228,7 +178,7 @@ catch_summary %>% ggplot() +
 #
 #scale_y_continuous(trans='log')
 
-ggsave('catchplot.png', height=10, width=15, units='in')
+
 
 dp_s = copy(ww_all)
 
@@ -245,6 +195,4 @@ ggplot(dp_s[below_lod==0 & below_loq==0, ]) +
   geom_ribbon(aes(x=day1, ymin=pred_lower, ymax=pred_upper), fill='red', alpha=0.2)+
   facet_wrap(~ara_id)+
   scale_y_continuous(trans='log')
-
-ggsave('ppp.png', height=10, width=15, units='in')
 
