01c7af2edc541d01d199b67c697ee397f6cde778
jmunday <jamesdanielmunday@gmail.com>
2024-03-13 13:56:01 +0100

    set run times

--- R/mw_201_spacetime_model_functional_server.R
+++ R/mw_201_spacetime_model_functional_server.R
@@ -94,7 +94,7 @@ ww_all = ww1 %>%
 saveRDS(ww_all,file=paste0(save.point,"/ww_all.rds"))
 
 
-ww_all = ww_all %>% filter(date > starts[i] & date < starts[i]+3)
+ww_all = ww_all %>% filter(date > starts[i] & date < starts[i] + 60)
 #ww_all = ww_all %>% filter(day1<20)
 
 ww_all = ww_all %>% complete(ara_id, day)
