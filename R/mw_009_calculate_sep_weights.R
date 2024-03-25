68beede2fdc2fb283de1f6cbff83a66f805e254c
jmunday <jamesdanielmunday@gmail.com>
2024-03-25 16:06:45 +0100

    add weights to sep calculations

--- R/mw_009_calculate_sep_weights.R
+++ R/mw_009_calculate_sep_weights.R
@@ -1,14 +1,18 @@
+library(tidyverse)
+library(data.table)
+library(sf)
+
 swissboundaries_BFS_NATION = st_read('data/spatial/bfs/swissboundaries3d_2023-01_2056_5728.shp/swissBOUNDARIES3D_1_4_TLM_LANDESGEBIET.shp')
 bfs_crs = st_crs(swissboundaries_BFS_NATION)
 pop_data = fread('data/population_statistics/STATPOP2021.csv')
 pop_data_slim = pop_data[,c('E_KOORD', 'N_KOORD',  'B21BTOT')]
 
 pop_sf = st_as_sf(pop_data_slim, coords = c("E_KOORD", "N_KOORD"), 
-                            crs = bfs_crs, remove=FALSE) %>% st_transform(crs = catchment_crs)
+                            crs = bfs_crs, remove=FALSE) 
 
 
 sep_sf = readRDS("../../02_data/sep3/ssep3_user_geo.Rds") %>% 
-  sf::st_transform(crs=catchment_crs)
+  sf::st_transform(crs=bfs_crs)
 
 
 joint_sf <- st_join(sep_sf, pop_sf, join = st_nearest_feature)
